41e262fe180e179325129a867588ef17d55c33c1
ReddPhoenix <wsguerrero@gmail.com>
2020-10-08 11:52:20 -0600

    created server and preliminary routes

--- routes.js
+++ routes.js
@@ -1,10 +1,21 @@
 // Route to HTML pages
 
+
 // Route to reach index.html
+app.get('/public/notes', (req, res) => {
+
+})
 
 // Route to reach notes.html
+app.get('/public/index.html', (req, res) => {
 
+})
 
 
 
 // Route to APIs
+// GET/POST/DELETE 
+
+app.get('/api/notes', req, res) => {
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// Module requirement for Express
 const express = require('express');
 
+// Express assigned to app variable
 const app = express();
 
+// Variable assigned to PORT for server
 const PORT = 3000;
 
+// Route to reach HTMLs & APIs
+require('/routes.js');
 
 
+//  Added listener for assigned PORT for server
 app.listen(PORT, () => {
     console.log("App running on PORT " + PORT);
 });
